a204d7e8e2dcdb01ca7ad249c0dee25f1de92dad
Israel Albarran <israelalbarran48@gmail.com>
2021-12-12 13:08:09 -0600

    Dashboard grid added

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -4,21 +4,123 @@ import './Dashboard.css';
 function Dashboard() {
     return (
       <div>
+          <div className="Welcome_section">
+              <h2>
+                    Hola Israel
+              </h2>
+
+
+              <h1>
+                  Preguntas UNAM
+
+              </h1>
+
+          </div> 
+
+
+
+
+          <div className="Status_section">
+
+              <div>
+                  <h4>
+                      Total de preguntas agregadas por materia:
+                  </h4>
+
+
+              </div>
+
+              <div>
+              <h3>
+                      Preguntas en Banco:
+                  </h3>
+
+              </div>
+
+
+ 
+
+
+
+
+
+          </div>
+
+
+
+
+
+          <div className="Materias_container1">
+
+<div>
+    hola
+
+</div>
+
+
+
+ <div>
+    
+</div>
+
+
+<div>
+
+</div>
+
+
+ <div>
+    
+</div>
+
+<div>
+    
+</div>
+
+
+
+
+</div>
+
+<div className="Materias_container1">
+
+<div>
+    hola
+
+</div>
+
+
+
+ <div>
+    
+</div>
+
+
+<div>
+
+</div>
+
+
+ <div>
+    
+</div>
+
+<div>
+    
+</div>
+
+
+
+
+</div>
+
+
+
+
+
 
       
-      <div>
-      <h1>Sign in/ Sign up</h1>
-          
-      <label for="fname">E-mail:</label>
-      <input type="text" id="fname" name="fname" ></input>
-      <label for="fname">Password:</label>
-      <input type="text" id="fname" name="fname" ></input>
-
-      <button type="button">Entrar</button>
-     
-      
-
-      </div>
+    
      
       
       
